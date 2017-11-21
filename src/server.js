24bb73e7a4ed54495e15046fd27941296034ed8d
coopyrightdmin <admin@coopyrightcode.com>
2017-11-21 12:40:21 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const recastBot = require('./bot');
+const config = require('./config');
+
+const onNewMessage = require('./message');
+
+const app = express();
+const port = config.PORT || 5000;
+app.set('port', port);
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+  recastBot.connect.handleMessage(req, res, onNewMessage);
+});
+
+app.listen(port, function() {
+  console.log(`App is listening on port ${port}`);
+});
